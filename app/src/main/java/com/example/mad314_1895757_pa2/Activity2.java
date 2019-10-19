14df77b2b7fc8f7bb86e6f25165411427de710df
Arora <1895757@winadpro.com>
2019-10-19 12:28:47 -0400

    Final Assignment Commit

---
+++ app/src/main/java/com/example/mad314_1895757_pa2/Activity2.java
@@ -0,0 +1,14 @@
+package com.example.mad314_1895757_pa2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
